723815bfcfe5e644ebe7f85d585df2a840eead0e
max dierschke <maxdierschke@maxs-MacBook-Air.fritz.box>
2022-07-03 00:22:54 +0200

    adapted some properties to files'
    '

--- .vimrc
+++ .vimrc
@@ -1,3 +1,22 @@
-set number
 syntax on
+
+set number
 set relativenumber
+
+set tabstop=4 softtabstop=4
+set shiftwidth-4
+set expandtab
+set smartindent
+
+set hidden
+
+set noerrorbells
+set nowrap
+
+set ignorecase
+set smartcase
+
+set incsearch
+set scrolloff=8
+set signcolumn=yes
+set colorcolumn=80
---
+++ linking.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -f ~/.aliases ] || [ -L ~/.aliases ]; then
+	echo '~/.aliases already exists and was not linked'
+else
+	ln -sf ~/.dotfiles/.aliases ~/.aliases  
+fi
+if [ -f ~/.vimrc ] || [ -L ~/.vimrc ]; then
+	echo '~/.vimrc already exists and was not linked'
+else
+	ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+fi
+if [ -f ~/.zshrc ] || [ -L ~/.zshrc ]; then
+	echo '~/.zshrc already exists and was not linked'
+else
+	ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+fi
+	
