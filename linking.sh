5b0150e907c82515baf658724781c599b57bced4
MDI <maxdierschke@maxs-Air.fritz.box>
2022-07-06 19:53:06 +0200

    added config file for tmux

--- linking.sh
+++ linking.sh
@@ -14,4 +14,9 @@ if [ -f ~/.zshrc ] || [ -L ~/.zshrc ]; then
 else
 	ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 fi
+if [ -f ~/.tmux.conf ] || [ -L ~/.tmux.conf ]; then
+	echo '~/.zshrc already exists and was not linked'
+else
+	ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+fi
 	
